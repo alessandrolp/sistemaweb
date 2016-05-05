ce6e7fa251d7242c721176e12374640ad09621e5
Alessandro Lopes Panaro <Alessandro Lopes Panaro>
2016-05-05 20:03:21 -0300

    ajustes phaselistener e hibernate.cfg

--- src/main/java/br/com/sistema/util/PhaseListenerSistemaweb.java
+++ src/main/java/br/com/sistema/util/PhaseListenerSistemaweb.java
@@ -6,42 +6,40 @@ import javax.faces.event.PhaseListener;
 
 import org.hibernate.Session;
 
-public class PhaseListenerSistemaweb implements PhaseListener{
+public class PhaseListenerSistemaweb implements PhaseListener {
 
-	// antes da fase
-	@Override
-	public void beforePhase(PhaseEvent fase) {
-		if(fase.getPhaseId().equals(PhaseId.RESTORE_VIEW)){
-			System.out.println("antes da fase: " + getPhaseId().toString());
-			Session session = HibernateUtil.getSessionFactory().openSession();
-			session.beginTransaction();
-			FacesContextUtil.setRequestSession(session);
-		}
-	}
-	
-	// depois da fase
-	@Override
-	public void afterPhase(PhaseEvent fase) {
-		System.out.println("depois da fase: " + getPhaseId().toString());
-		if(fase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)){
-			Session session = FacesContextUtil.getRequestSession();
-			try {
-				session.getTransaction().commit();
-			} catch (Exception e) {
-				if(session.getTransaction().isActive()){
-					session.getTransaction().rollback();
-				}
-			} finally{
-				session.close();
-			}
-		}
-	}
+    // antes da fase
+    @Override
+    public void beforePhase(PhaseEvent fase) {
+        System.out.println("Antes da fase: " + fase.getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            FacesContextUtil.setRequestSession(session);
+        }
+    }
 
-	@Override
-	public PhaseId getPhaseId() {
-		return PhaseId.ANY_PHASE;
-	}
-	
+    // depois da fase
+    @Override
+    public void afterPhase(PhaseEvent fase) {
+        System.out.println("Depois da fase: " + fase.getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+            Session session = FacesContextUtil.getRequestSession();
+            try {
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } finally {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+
 }
-
-
